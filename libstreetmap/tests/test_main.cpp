a86db9ceda5e124b2a4dd5cf105cd4c8dfcc5608
danny <danial.ahmad@mail.utoronto.ca>
2021-02-06 13:37:27 -0500

    created UTest file

---
+++ libstreetmap/tests/test_main.cpp
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   test_main.cpp
+ * Author: ahmadda4
+ *
+ * Created on February 6, 2021, 1:24 PM
+ */
+
+#include <iostream>
+#include "m1.h"
+#include "LatLon.h"
+
+int testClosestPOI(){
+    
+    
+    return 0;
+}
+
+int main() {
+    
+    int error = 0;
+    
+    error += testClosestPOI();
+    
+    return 0;
+}
+
