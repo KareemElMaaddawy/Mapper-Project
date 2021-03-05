e3a6b02176221b7bc7dee9c168964beeb6289879
Kareem <kareem.elmaaddawy@mail.utoronto.ca>
2021-03-05 00:12:35 -0500

    Added function to draw canvas

--- libstreetmap/src/m2.cpp
+++ libstreetmap/src/m2.cpp
@@ -2,6 +2,8 @@
 #include "ezgl/application.hpp"
 #include "ezgl/graphics.hpp"
 
+void drawMainCanvas (ezgl::renderer *g);
+
 void drawMap(){
     ezgl::application::settings settings;
     settings.main_ui_resource = "libstreetmap/resources/main.ui";
@@ -11,11 +13,16 @@ void drawMap(){
     ezgl::application application(settings);
     
     
-    
+    ezgl::rectangle initial_world({0,0}, {1000,1000});
+    application.add_canvas("MainCanvas", drawMainCanvas, initial_world);
     
     
     
     
     
     application.run(nullptr, nullptr, nullptr, nullptr);
+}
+
+void drawMainCanvas (ezgl::renderer *g){
+    g -> draw_rectangle ({0,0},{1000,1000});
 }
