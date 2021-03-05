b7ee9a6135c58e56791c49f021829b65a2a457dc
Ghamr Saeed <ghamr.saeed@mail.utoronto.ca>
2021-03-05 09:09:52 -0500

    m2 displays toronto with lat long. Next, I'll try to display it with x,y

--- libstreetmap/src/m2.cpp
+++ libstreetmap/src/m2.cpp
@@ -8,10 +8,24 @@ struct intersection_data {
   LatLon position;
   std::string name;
 }typedef intersection_data;
-
+std::vector<intersection_data> intersections;
 
 void drawMap () {
-    
+    double max_lat = getIntersectionPosition(0).latitude(); 
+    double min_lat = max_lat;
+    double max_lon = getIntersectionPosition(0).longitude();
+    double min_lon = max_lon;
+    intersections.resize(getNumIntersections());
+
+    for (int id = 0; id < getNumIntersections(); ++id) {  
+      intersections[id].position = getIntersectionPosition(id);
+      intersections[id].name = getIntersectionName(id);
+
+      max_lat = std::max(max_lat, intersections[id].position.latitude());
+      min_lat = std::min(min_lat, intersections[id].position. latitude());
+      max_lon = std::max(max_lon, intersections[id].position.longitude());
+      min_lon = std::min(min_lon, intersections[id].position.longitude());
+    }
 
     ezgl::application::settings settings;
     settings.main_ui_resource = "libstreetmap/resources/main.ui";
@@ -20,8 +34,10 @@ void drawMap () {
     
     ezgl::application application(settings);
     
+   
     
-    ezgl::rectangle initial_world({0,0}, {1000,1000});
+    ezgl::rectangle initial_world({min_lon, min_lat},
+                              {max_lon, max_lat});
     application.add_canvas("MainCanvas", drawMainCanvas, initial_world);
     
     
@@ -32,13 +48,14 @@ void drawMap () {
 }
 
 void drawMainCanvas(ezgl::renderer *g) { 
-  g->draw_rectangle({0, 0}, {1000, 1000});
  
-  std::vector<intersection_data> intersections;
-
-
-  intersections.resize(getNumIntersections());
+ 
   
+
+
+  
+  
+
   for (int id = 0; id < getNumIntersections(); ++id) {
     intersections[id].position = getIntersectionPosition(id);   
     intersections[id].name = getIntersectionName(id);
@@ -48,7 +65,7 @@ void drawMainCanvas(ezgl::renderer *g) {
     float x = intersections[i].position.longitude();
     float y = intersections[i].position.latitude();
 
-    float width = 10;
+    float width = 0.001;
     float height = width;
 
     g->fill_rectangle({x, y}, {x + width, y + height});
