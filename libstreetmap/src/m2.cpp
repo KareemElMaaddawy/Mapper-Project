86f75efbef94982e4b10bce845d5976c43c7773f
Ghamr Saeed <ghamr.saeed@mail.utoronto.ca>
2021-03-05 10:55:40 -0500

    Implemented the highlight on click functionality. Mouse now highlights closest intersections at all times. not sure why this happens

--- libstreetmap/src/m2.cpp
+++ libstreetmap/src/m2.cpp
@@ -2,26 +2,56 @@
 #include "m1.h"
 #include "ezgl/application.hpp"
 #include "ezgl/graphics.hpp"
-//definitions
+//function declarations
 void drawMainCanvas(ezgl::renderer *g);
-double x_from_lon(float lon);
+double x_from_lon(double lon);
 double y_from_lat(double lat);
+double lon_from_x(double x);
+double lat_from_y(double lat);
+void act_on_mouse_click(ezgl::application* app,   
+                        GdkEventButton* event,
+                        double x, double y); 
+//variable declarations
+struct intersection_data {
+    LatLon position;
+    std::string name;
+    bool highlight = false;
+}typedef intersection_data;
+
+std::vector<intersection_data> intersections;
 double avg_lat = 0;
 
-//declarations
-struct intersection_data {
-  LatLon position;
-  std::string name;
-}typedef intersection_data;
-std::vector<intersection_data> intersections;
-
 //implementations
-double x_from_lon(float lon){
+double x_from_lon(double lon){
     return lon * kDegreeToRadian * kEarthRadiusInMeters * std::cos(avg_lat * kDegreeToRadian);
 }
 
 double y_from_lat(double lat){
     return lat * kDegreeToRadian * kEarthRadiusInMeters;
+}
+
+double lon_from_x(double x){
+    return x/(kDegreeToRadian * kEarthRadiusInMeters * std::cos(avg_lat * kDegreeToRadian));
+}
+
+double lat_from_y(double lon){
+    return lon/(kDegreeToRadian * kEarthRadiusInMeters);
+}
+
+
+
+void act_on_mouse_click(ezgl::application* app,   
+                        GdkEventButton* event,
+                        double x, double y) {
+  std::cout << "Mouse clicked at (" << x << "," << y << ")\n";
+  std::cout << "Button clicked is " << event->button << std::endl;
+  LatLon pos = LatLon(lat_from_y(y), lon_from_x(x)); 
+  int id = findClosestIntersection(pos); 
+
+  std::cout << "Closest Intersection: "
+            << intersections[id].name << "\n"; 
+  intersections[id].highlight = true;
+  app->refresh_drawing();
 }
 
 void drawMap () {
@@ -60,12 +90,12 @@ void drawMap () {
     
     
     
-    application.run(nullptr, nullptr, nullptr, nullptr);
+    application.run(nullptr, nullptr, act_on_mouse_click, nullptr);
 }
 
 void drawMainCanvas(ezgl::renderer *g) { 
  
-    g->set_color(ezgl::GREEN);
+    
     for (int id = 0; id < getNumIntersections(); ++id) {
         intersections[id].position = getIntersectionPosition(id);   
         intersections[id].name = getIntersectionName(id);
@@ -74,7 +104,13 @@ void drawMainCanvas(ezgl::renderer *g) {
     for (int i = 0; i < intersections.size(); ++i) {
         float x = x_from_lon(intersections[i].position.longitude());
         float y = y_from_lat(intersections[i].position.latitude());
-
+        
+        if (intersections[i].highlight) {
+            g->set_color(ezgl::RED);
+          } else {
+            g->set_color(ezgl::GREY_55);
+          }
+        
         float width = 100;
         float height = width;
 
