f5911f6c2c721cad39c44041eeb1efba12f3f159
Ghamr Saeed <ghamr.saeed@mail.utoronto.ca>
2021-03-05 08:34:15 -0500

    m2 displays toronto as a tiny square as in the walkthrough. Could not get it to display the correct version

--- libstreetmap/src/m2.cpp
+++ libstreetmap/src/m2.cpp
@@ -1,10 +1,18 @@
 #include "m2.h"
+#include "m1.h"
 #include "ezgl/application.hpp"
 #include "ezgl/graphics.hpp"
+void drawMainCanvas(ezgl::renderer *g);
 
-void drawMainCanvas (ezgl::renderer *g);
+struct intersection_data {
+  LatLon position;
+  std::string name;
+}typedef intersection_data;
 
-void drawMap(){
+
+void drawMap () {
+    
+
     ezgl::application::settings settings;
     settings.main_ui_resource = "libstreetmap/resources/main.ui";
     settings.window_identifier = "MainWindow";
@@ -23,6 +31,26 @@ void drawMap(){
     application.run(nullptr, nullptr, nullptr, nullptr);
 }
 
-void drawMainCanvas (ezgl::renderer *g){
-    g -> draw_rectangle ({0,0},{1000,1000});
+void drawMainCanvas(ezgl::renderer *g) { 
+  g->draw_rectangle({0, 0}, {1000, 1000});
+ 
+  std::vector<intersection_data> intersections;
+
+
+  intersections.resize(getNumIntersections());
+  
+  for (int id = 0; id < getNumIntersections(); ++id) {
+    intersections[id].position = getIntersectionPosition(id);   
+    intersections[id].name = getIntersectionName(id);
+  }
+  
+  for (int i = 0; i < intersections.size(); ++i) {
+    float x = intersections[i].position.longitude();
+    float y = intersections[i].position.latitude();
+
+    float width = 10;
+    float height = width;
+
+    g->fill_rectangle({x, y}, {x + width, y + height});
+  }
 }
