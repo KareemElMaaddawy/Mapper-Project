fcc8eddbc0da13c90d5e3ac3056c7f6a2f56b866
Ghamr Saeed <ghamr.saeed@mail.utoronto.ca>
2021-03-05 10:01:52 -0500

    converted lan lon to x,y. map has the correct scale. width is 100 instead of 0.001

--- libstreetmap/src/m2.cpp
+++ libstreetmap/src/m2.cpp
@@ -2,13 +2,27 @@
 #include "m1.h"
 #include "ezgl/application.hpp"
 #include "ezgl/graphics.hpp"
+//definitions
 void drawMainCanvas(ezgl::renderer *g);
+double x_from_lon(float lon);
+double y_from_lat(double lat);
+double avg_lat = 0;
 
+//declarations
 struct intersection_data {
   LatLon position;
   std::string name;
 }typedef intersection_data;
 std::vector<intersection_data> intersections;
+
+//implementations
+double x_from_lon(float lon){
+    return lon * kDegreeToRadian * kEarthRadiusInMeters * std::cos(avg_lat * kDegreeToRadian);
+}
+
+double y_from_lat(double lat){
+    return lat * kDegreeToRadian * kEarthRadiusInMeters;
+}
 
 void drawMap () {
     double max_lat = getIntersectionPosition(0).latitude(); 
@@ -22,10 +36,12 @@ void drawMap () {
       intersections[id].name = getIntersectionName(id);
 
       max_lat = std::max(max_lat, intersections[id].position.latitude());
-      min_lat = std::min(min_lat, intersections[id].position. latitude());
+      min_lat = std::min(min_lat, intersections[id].position.latitude());
       max_lon = std::max(max_lon, intersections[id].position.longitude());
       min_lon = std::min(min_lon, intersections[id].position.longitude());
     }
+    
+    avg_lat = (min_lat + max_lat)/2;
 
     ezgl::application::settings settings;
     settings.main_ui_resource = "libstreetmap/resources/main.ui";
@@ -36,8 +52,8 @@ void drawMap () {
     
    
     
-    ezgl::rectangle initial_world({min_lon, min_lat},
-                              {max_lon, max_lat});
+    ezgl::rectangle initial_world({x_from_lon(min_lon), y_from_lat(min_lat)},
+                              {x_from_lon(max_lon), y_from_lat(max_lat)});
     application.add_canvas("MainCanvas", drawMainCanvas, initial_world);
     
     
@@ -49,25 +65,19 @@ void drawMap () {
 
 void drawMainCanvas(ezgl::renderer *g) { 
  
- 
+    g->set_color(ezgl::GREEN);
+    for (int id = 0; id < getNumIntersections(); ++id) {
+        intersections[id].position = getIntersectionPosition(id);   
+        intersections[id].name = getIntersectionName(id);
+    }
   
+    for (int i = 0; i < intersections.size(); ++i) {
+        float x = x_from_lon(intersections[i].position.longitude());
+        float y = y_from_lat(intersections[i].position.latitude());
 
+        float width = 100;
+        float height = width;
 
-  
-  
-
-  for (int id = 0; id < getNumIntersections(); ++id) {
-    intersections[id].position = getIntersectionPosition(id);   
-    intersections[id].name = getIntersectionName(id);
-  }
-  
-  for (int i = 0; i < intersections.size(); ++i) {
-    float x = intersections[i].position.longitude();
-    float y = intersections[i].position.latitude();
-
-    float width = 0.001;
-    float height = width;
-
-    g->fill_rectangle({x, y}, {x + width, y + height});
-  }
+        g->fill_rectangle({x, y}, {x + width, y + height});
+    }
 }
