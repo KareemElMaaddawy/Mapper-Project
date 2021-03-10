49c3043a35377eeb981eb1e6518e9b3b2aba5bda
Ghamr Saeed <ghamr.saeed@mail.utoronto.ca>
2021-03-10 14:57:35 -0500

    included global and header in multiple files

--- libstreetmap/src/ezgl/globalHeader.h
+++ libstreetmap/src/ezgl/globalHeader.h
@@ -10,11 +10,22 @@
  *
  * Created on March 6, 2021, 1:50 AM
  */
+#include <vector>
+#include "LatLon.h" 
+#include "OSMID.h"
+#include "m1.h"
+#include "m2.h"
 
-#ifndef GLOBALCLASSES_H
-#define GLOBALCLASSES_H
+#pragma once
+extern std::vector<std::vector<LatLon>> points_on_segments;
 
+extern std::vector<std::vector<std::pair<float, float>>> xy_points_segments;
 extern double aspVar;
+extern double avg_lat;
+extern double avg_lon;
 
-#endif /* GLOBALCLASSES_H */
+double x_from_lon(double lon);
+double y_from_lat(double lat);
+
+
 
